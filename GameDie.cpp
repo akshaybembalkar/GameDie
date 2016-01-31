61e5334ade2ffa478d8d43b0c12150c396279cfe
akshaybembalkar <abembalkar@mail.csuchico.edu>
2016-01-31 11:30:21 -0800

    finalCommit

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <cstdlib>
+#include<time.h>
+
+using namespace std;
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+	srand(time(NULL));
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+	cout<< rand() % 10 + 1<<endl;
 }
+
+
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "GameDie.h"
+
+using namespace std;
+
+int main()
+{
+	GameDie obj;
+	obj.roll();
+
+	return 0;
+}
